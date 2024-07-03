7684c31e087ef6aa34de98bad373fe4b062b0fd8
Nicolas Verdugo <nicolas.verdugo@usm.cl>
2024-07-03 19:49:59 -0400

    Control 3
    Nicolás Verdugo
    202012033-1

--- src/domain/historical_events/use_cases/getHistoricalEvents.js
+++ src/domain/historical_events/use_cases/getHistoricalEvents.js
@@ -1,6 +1,41 @@
 import historicalEventsRepository from '../repository/historicalEventsRepository'
 
 exports.getHistoricalEventsByOcurrence = (ctx) => {
-    ctx.body = historicalEventsRepository.getHistoricalEvents(ctx.params.ocurrence)
+    try 
+    {
+    const ocurrence = ctx.params.ocurrence;
+    console.log("ocurrence fuera:",ocurrence);
+    console.log(ocurrence.length);
+    if (ocurrence.length === 2)
+        {
+        const temp1=parseInt(ocurrence[0]);
+        const temp2=parseInt(ocurrence[1]);
+        const dato1=temp1.toString();
+        const dato2=temp2.toString();
+        console.log("datos: ",typeof(dato1)," y ",typeof(dato2));
+        console.log("datos: ",dato1," y ",dato2);
+        if(dato1==="NaN" && dato2==="NaN")
+            {
+            if(ocurrence==="ac"||ocurrence==="dc")
+                {
+                ctx.body = historicalEventsRepository.getHistoricalEvents(ocurrence)
+                }else{
+                    ctx.status = 400;
+                    ctx.body = { message:"El input debe ser ac o dc"};
+                    }
+            }else{
+                ctx.status = 400;
+                ctx.body = { message:"Solo se aceptan caracteres no numéricos"};
+                }
+
+        }else{
+            ctx.status = 400;
+            ctx.body = { message:"El input debe ser == 2" };
+            }
     return ctx
+    }catch (error) {
+    console.error('Error al procesar la solicitud:', error);
+    ctx.status = 500;
+    ctx.body = { error: 'Error interno del servidor' };
+}
 }
--- tests/routes/health/GETHealth.integration.test.js
+++ tests/routes/health/GETHealth.integration.test.js
@@ -16,3 +16,38 @@ describe('GET /health', () => {
         expect(response.body).toEqual({ message: 'ok' })
     })
 })
+
+describe('consulta GET /api/history/:ocurrence', () => {
+    test('Debería devolver un arreglo con los eventos históricos que hayan resultado de la búsqueda ordenados desde el más antiguo al más nuevo, en donde "date" siempre es <= 0', async () => {
+        const response = await request(app.callback()).get('/api/history/ac')
+        expect(response.status).toBe(200)
+        expect(Array.isArray(response.body)).toBe(true)
+    })
+    
+    test('Debería devolver un arreglo con los eventos históricos que hayan resultado de la búsqueda ordenados desde el más antiguo al más nuevo, en donde "date" siempre es > 0', async () => {
+        const response = await request(app.callback()).get('/api/history/dc')
+        expect(response.status).toBe(200)
+        expect(Array.isArray(response.body)).toBe(true)
+    })
+    test('Debería devolver el mensaje "Solo se aceptan caracteres no numéricos" al ingresar un número con letras', async () => {
+        const response = await request(app.callback()).get('/api/history/ad')
+        expect(response.body).toEqual({ message: "El input debe ser ac o dc" })
+        expect(response.status).toBe(400)
+    })
+    test('Debería devolver el mensaje "Solo se aceptan caracteres no numéricos" al ingresar un número', async () => {
+        const response = await request(app.callback()).get('/api/history/a4')
+        expect(response.body).toEqual({ message: 'Solo se aceptan caracteres no numéricos' })
+        expect(response.status).toBe(400)
+    })
+    test('Debería devolver el mensaje "Solo se aceptan caracteres no numéricos" al ingresar un número con letras', async () => {
+        const response = await request(app.callback()).get('/api/history/25')
+        expect(response.body).toEqual({ message: 'Solo se aceptan caracteres no numéricos' })
+        expect(response.status).toBe(400)
+    })
+    test('Debería devolver el mensaje "El input debe ser ac o dc" y el largo sea != 2', async () => {
+        const response = await request(app.callback()).get('/api/history/ac1')
+        expect(response.body).toEqual({ message: "El input debe ser == 2" })
+        expect(response.status).toBe(400)
+    })
+
+})
